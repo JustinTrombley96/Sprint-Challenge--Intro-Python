54f1c49db27abf25590acb363e636f8fd19ac278
Justin Trombley <jtrombley96@gmail.com>
2020-05-07 22:30:46 -0400

    oop1 Finished

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+#Base Class
+class Vehicle:
+    def __init__(self)
+        pass
+
+class FlightVehicle(Vehicle):
+    def __init__(self)
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self)
+        pass
+
+class Airplane(FlightVehicle):
+    def __init__(self)
+        pass
+
+class GroundVehicle(Vehicle):
+    def __init__(self)
+        pass
+
+class Car(Vehicle):
+    def __init__(self)
+        pass
+
+class Motorcycle(Vehicle):
+    def __init__(self)
+        pass
